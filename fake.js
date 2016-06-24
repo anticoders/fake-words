633b893460f2ca68b568c3f2bc180b749f766306
Raiyan Kamal <raiyan.kamal@gmail.com>
2016-06-23 21:50:09 -0400

    Relocated constants inside the function

--- fake.js
+++ fake.js
@@ -271,19 +271,18 @@ var _getRandomNumber = function (min, max, isInteger) {
   return r;
 };
 
-var MAX_INT = 9007199254740991;
-var MIN_INT = -9007199254740991;
-
 Fake.simpleSchemaDoc = function(schema) {
+  var _MAX_INT = 9007199254740991;
+  var _MIN_INT = -9007199254740991;
   var fakeObj = {};
   _.each(schema._schemaKeys, function (key) {
     var schemaKey = schema._schema[key],
         type = schema._schema[key].type.name,
-        max = _.get(schemaKey, 'max', MAX_INT),
-        min = _.get(schemaKey, 'min', MIN_INT),
+        max = _.get(schemaKey, 'max', _MAX_INT),
+        min = _.get(schemaKey, 'min', _MIN_INT),
         allowedValues = _.get(schemaKey, 'allowedValues', undefined),
         value = null;
-    min = _.clamp(min, MIN_INT, max);
+    min = _.clamp(min, _MIN_INT, max);
     switch(type) {
       case 'String':
         if (allowedValues) {
