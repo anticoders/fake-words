95f9a42310efeb351d140a5471a0508dc408e493
lawrentiy <lawrentiy@gmail.com>
2016-09-20 14:58:33 +0300

    Add test for Object field type

--- tests/simpleSchemaDoc.test.js
+++ tests/simpleSchemaDoc.test.js
@@ -34,3 +34,18 @@ Tinytest.add('muqube:fake - simpleSchemaDoc - Respects max and min limits of Num
     });
 
 });
+
+Tinytest.add('muqube:fake - simpleSchemaDoc - Create inner Object', (test) => {
+    let s = new SimpleSchema({f4: {type: Number}});
+    let s2 = new SimpleSchema({
+        f1: {type: String},
+        f2: {type: Object},
+        f3: {type: s}
+    });
+    let doc = Fake.simpleSchemaDoc(s2);
+
+    test.isTrue(_.isString(doc.f1));
+    test.isTrue(_.isObject(doc.f2));
+    test.isTrue(_.isObject(doc.f3));
+    test.isTrue(_.isNumber(doc.f3.f4));
+});
